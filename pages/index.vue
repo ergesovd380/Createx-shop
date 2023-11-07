787433ae78437e6f27e3ae3d3b9b19b25ad015ec
ergesovd380 <ergesovd380@gmail.com>
2023-11-07 19:20:17 +0300

    Subscribe page

--- pages/index.vue
+++ pages/index.vue
@@ -371,6 +371,10 @@ const brands = ref([
   'images/home-page/brands/img-6.png'
 ]);
 
+// Subscribe page
+const subscribeBtns = ref(['Women', 'Men', 'Girls', 'Boys']);
+const subscribeSelection = ref([]);
+
 // Desktop size
 const calculateNewarrival = () => {
   if(window.innerWidth < 600) {
@@ -746,21 +750,21 @@ onMounted(() => {
 <!-- Form -->
             <v-form 
               @submit.prevent
-              class="home-page__gallery-form"
+              class="form"
             >
               <label 
                 for="email__gallery" 
-                class="home-page__gallery-form-label"
+                class="form__label base base__regular"
               >
                 Email
               </label>
               <InputText 
                 placeholder="Your working email" 
-                class="home-page__gallery-form-input"
+                class="form__input"
                 id="email__gallery"
               />
               <Btn class="btn__small">Subscribe</Btn>
-              <div class="home-page__gallery-form-singup">*Sign up to be the first to hear about exclusive deals, special offers and upcoming collections.</div>
+              <div class="form__singup">*Sign up to be the first to hear about exclusive deals, special offers and upcoming collections.</div>
             </v-form>
           </v-img>
         </v-col>
@@ -1188,12 +1192,53 @@ onMounted(() => {
                 home-page__subscribe-subtitle
               "
             >Subscribe for exclusive early sale access and new arrivals.</div>
-            <div class="home-page__subscribe-type">
-              
+            <div>
+              <v-item-group 
+                v-model="subscribeSelection" 
+                multiple 
+                selected-class="home-page__subscribe-selected"
+              >
+                <v-item
+                  v-for="(btn, indexSubscribe) in subscribeBtns"
+                  :key="indexSubscribe"
+                  v-slot="{ selectedClass, toggle }"
+                >
+                  <BtnOutline
+                    :class="selectedClass"
+                    class="btn__subscribe-selection"
+                    @click="toggle"
+                  >
+                    {{ btn }}
+                  </BtnOutline>
+                </v-item>
+              </v-item-group>
             </div>
+<!-- Form -->
+            <v-form 
+              @submit.prevent
+              class="form"
+            >
+              <label 
+                for="email__gallery" 
+                class="form__label base base__regular"
+              >
+                Email
+              </label>
+              <InputText 
+                placeholder="Your working email" 
+                class="form__input"
+                id="email__gallery"
+              />
+              <Btn class="btn__small">Subscribe</Btn>
+              <v-checkbox 
+                color="var(--primary)" 
+                class="form__checkbox"
+                label="I agree to receive communications from Createx Store.">
+              </v-checkbox>
+            </v-form>
           </v-col>
-          <v-col>
-            <h1>Hello World</h1>
+          <v-col class="home-page__subscribe-col">
+            <img src="images/home-page/subscribe/img.png">
           </v-col>
         </v-row>
       </v-container>
